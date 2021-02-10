b8797889b6075e95c9e0675aa1091ef1849b801e
Cinnamon <cinnamon_bun_github@fastmail.com>
2021-02-09 16:02:09 -0800

    set up package infrastructure

---
+++ src/test/hello.test.ts
@@ -0,0 +1,9 @@
+import t = require('tap');
+//t.runOnly = true;
+
+//================================================================================
+
+t.test('hello world', async (t: any) => {
+    t.ok(true, 'hello world');
+    t.done();
+});
